11b61a28277fbfa94d40fc49a834dad831a52a23
rumigor <rumigor@gmail.com>
2020-08-28 15:51:30 +0300

    Fragment version

--- app/src/main/java/rumigor/lesson1/SettingsActivity.java
+++ app/src/main/java/rumigor/lesson1/SettingsActivity.java
@@ -7,6 +7,7 @@ import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.app.Application;
 import android.content.Intent;
+import android.content.res.Configuration;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
@@ -27,6 +28,8 @@ public class SettingsActivity extends AppCompatActivity implements Constants {
     boolean nightMode;
     boolean celsius = true;
     ConstraintLayout layout;
+    SettingsFragment settingsFragment;
+    String SF = "settingsFrame";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,104 +38,22 @@ public class SettingsActivity extends AppCompatActivity implements Constants {
         Toast.makeText(getApplicationContext(), "onCreate#3", Toast.LENGTH_SHORT).show();
         Log.d("SettingsActivity", "onCreate");
         setContentView(R.layout.activity_settings);
-        theme = findViewById(R.id.nightTheme);
-        layout = findViewById(R.id.backgroundLayout);
-        nightMode = getIntent().getExtras().getBoolean(NIGHT_THEME);
-        celsius = getIntent().getExtras().getBoolean(CELSIUS);
-        RadioButton c = findViewById(R.id.celcius);
-        RadioButton f = findViewById(R.id.fahrenheit);
-        if (nightMode){
-            theme.setChecked(true);
-            layout.setBackgroundColor(Color.rgb(0, 85, 124));
+        if (getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE) {
+            finish();
         }
-        if (celsius) {
-            c.setChecked(true);
+        if (savedInstanceState != null) { // saved instance state, fragment may exist
+            // look up the instance that already exists by tag
+            settingsFragment = (SettingsFragment)
+                    getSupportFragmentManager().findFragmentByTag(SF);
+        } else if (settingsFragment == null) {
+            // only create fragment if they haven't been instantiated already
+            settingsFragment = new SettingsFragment();
         }
-        else f.setChecked(true);
-        Button cancel = findViewById(R.id.resetButton);
-        cancel.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                finish();
-            }
-        });
-        Button saveSet = findViewById(R.id.saveButton);
-        saveSet.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent();
-                nightMode = theme.isChecked();
-                celsius = c.isChecked();
-                intent.putExtra(NIGHT_THEME, nightMode).putExtra(CELSIUS, celsius);
-                setResult(100, intent);
-                finish();
-            }
-        });
-        theme.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if (theme.isChecked()) {
-                    layout.setBackgroundColor(Color.rgb(0, 85, 124));
-                }
-                else {
-                    layout.setBackgroundColor(Color.argb(100, 164, 221, 248));
-                }
-            }
-        });
-    }
-    @Override
-    protected void onStart() {
-        super.onStart();
-        Toast.makeText(getApplicationContext(), "onStart#3", Toast.LENGTH_SHORT).show();
-        Log.d("SettingsActivity", "onStart");
-    }
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-        Toast.makeText(getApplicationContext(), "onStop#3", Toast.LENGTH_SHORT).show();
-        Log.d("SettingsActivity", "onStop");
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        Toast.makeText(getApplicationContext(), "onDestroy#3", Toast.LENGTH_SHORT).show();
-        Log.d("SettingsActivity", "onDestroy");
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        Toast.makeText(getApplicationContext(), "onPause#3", Toast.LENGTH_SHORT).show();
-        Log.d("SettingsActivity", "onPause");
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        Toast.makeText(getApplicationContext(), "onResume#3", Toast.LENGTH_SHORT).show();
-        Log.d("SettingsActivity", "onResume");
-    }
-
-    @Override
-    protected void onSaveInstanceState(@NonNull Bundle outState) {
-        super.onSaveInstanceState(outState);
-        outState.putBoolean(NIGHT_MODE, theme.isChecked());
-    }
-
-    @Override
-    protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
-        super.onRestoreInstanceState(savedInstanceState);
-        boolean status = savedInstanceState.getBoolean(NIGHT_MODE);
-        if (status) {
-            theme.setChecked(true);
-            layout.setBackgroundColor(Color.BLUE);
-        }
-        else {
-            theme.setChecked(false);
-            layout.setBackgroundColor(Color.argb(100, 164, 221, 248));
+        if (!settingsFragment.isInLayout()) {
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.settingsFragment, settingsFragment, SF)
+                    .commit();
         }
     }
-
 }
